e0986cfe16fb68031c9a068819f89acf7215c10e
FelixBla <fxb321@gmail.com>
2015-06-21 21:41:57 +0200

    removed message when cached data exists

--- cachematrix.R
+++ cachematrix.R
@@ -26,7 +26,6 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
     i <- x$getinverse()
     if(!is.null(i)) {
-        message("getting cached data")
         return(i)
     }
     data <- x$get()
